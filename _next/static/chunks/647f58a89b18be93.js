07fe3ffcbbc0a5fcf852ee5ea97d3c94660766eb
techkingishere <emirberaarslan@gmail.com>
2025-10-01 19:09:33 -0400

    fix(pages): publish from repo root to match Pages folder setting

---
+++ _next/static/chunks/647f58a89b18be93.js
@@ -0,0 +1,5 @@
+__turbopack_load_page_chunks__("/_error", [
+  "static/chunks/17722e3ac4e00587.js",
+  "static/chunks/193a289de790f218.js",
+  "static/chunks/turbopack-520681ff0e309475.js"
+])
---
+++ _next/static/chunks/72d5d127e6e3eeb3.js
@@ -0,0 +1,5 @@
+__turbopack_load_page_chunks__("/_app", [
+  "static/chunks/e60ef129113f6e24.js",
+  "static/chunks/193a289de790f218.js",
+  "static/chunks/turbopack-b468a3a7778cc388.js"
+])
